b6c27f309bf555e49b22b490a9583d16fcb87f5b
Ohad Porat <ohadp23@gmail.com>
2021-03-24 15:43:59 -0400

    commented out music

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -14,18 +14,18 @@ export default class Preloader extends Phaser.Scene {
     ])
     this.load.addFile(fonts)
 
-    this.load.audio('game-music', 'assets/music/8bittribute.wav')
-    this.load.audio('confirmation', 'assets/sfx/confirmation_001.wav')
-    this.load.audio('move', 'assets/sfx/maximize_008.wav')
-    this.load.audio('error', 'assets/sfx/error_006.wav')
-    this.load.audio('click', 'assets/sfx/click2.wav')
+    // this.load.audio('game-music', 'assets/music/8bittribute.wav')
+    // this.load.audio('confirmation', 'assets/sfx/confirmation_001.wav')
+    // this.load.audio('move', 'assets/sfx/maximize_008.wav')
+    // this.load.audio('error', 'assets/sfx/error_006.wav')
+    // this.load.audio('click', 'assets/sfx/click2.wav')
   }
 
   create() {
-    this.sound.play('game-music', {
-      loop: true,
-      volume: 0.05
-    })
+    // this.sound.play('game-music', {
+    //   loop: true,
+    //   volume: 0.05
+    // })
 
     this.scene.start('game', { level: 1 })
   }
