9767dcbe5dad19aa650b0285b71ed0fdd2a52e33
Ross Hurlock <jrlhurlock@gmail.com>
2021-03-25 07:50:03 -0400

    created level 8

--- src/levels/levelService.ts
+++ src/levels/levelService.ts
@@ -5,6 +5,7 @@ import level4 from './level4'
 import level5 from './level5'
 import level6 from './level6'
 import level7 from './level7'
+import level8 from './level8'
 
 class LevelService {
   private levels = [
@@ -14,7 +15,8 @@ class LevelService {
     level4,
     level5,
     level6,
-    level7
+    level7,
+    level8
   ]
 
   getLevel(level: number) {
