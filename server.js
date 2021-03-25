b47cbe6dcf7a9220777616cf559ca39c392e4307
Ross Hurlock <jrlhurlock@gmail.com>
2021-03-25 17:01:36 -0400

    server.js mods

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+const PORT = process.env.PORT || 8080
+
+const buildPath = path.resolve(__dirname, 'build');
+
+app.use(express.static(__dirname + '/build'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
+
+app.listen(PORT);
