f301ef0f543b6b6eebc2e40e90f88278358fa281
Ross Hurlock <jrlhurlock@gmail.com>
2021-03-25 07:05:20 -0400

    added level 5

--- src/levels/levelService.ts
+++ src/levels/levelService.ts
@@ -2,13 +2,15 @@ import level1 from './level1'
 import level2 from './level2'
 import level3 from './level3'
 import level4 from './level4'
+import level5 from './level5'
 
 class LevelService {
   private levels = [
     level1,
     level2,
     level3,
-    level4
+    level4,
+    level5
   ]
 
   getLevel(level: number) {
